8577bbd73d36bbe219deb38ba98d2315c203e8ac
Aimopotis <morpheustudio@gmail.com>
2018-06-12 19:46:11 +0300

    New things

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,6 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
-import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
@@ -13,8 +12,8 @@ import { EditServerComponent } from './servers/edit-server/edit-server.component
 import { ServerComponent } from './servers/server/server.component';
 import { ServersService } from './servers/servers.service';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
-
-
+import { AppRoutingModule } from './app-routing.module';
+import { RouterModule, Routes } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', component: HomeComponent },
@@ -27,7 +26,6 @@ const appRoutes: Routes = [
   ] },
     {path:'not-found', component: PageNotFoundComponent},
     {path:'**', redirectTo: '/not-found'}
-  
 ];
 
 
